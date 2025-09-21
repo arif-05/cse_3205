a0be1ac5e612acc0baf694d7d462631f4391bdb3
arif-05 <arifcse949@gmail.com>
2025-09-21 15:17:02 +0600

    created another branch

---
+++ another.cpp
@@ -0,0 +1,10 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World!"<<endl;
+    cout<<"Hey, Another!"<<endl;
+    return 0;
+}
